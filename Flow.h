12d66280ba9fc0cbb38b6a1a43182e4b426fe3d1
Ali Lasemi <alasem2@linux-a3.ews.illinois.edu>
2018-11-12 16:49:51 -0600

    Implemented Case 2 of Riemann problem
    Added a function for calculating rho, u, and p from Case 2 of the
    Riemann problem, as defined by Knight.

--- Flow.h
+++ Flow.h
@@ -32,6 +32,7 @@ class Flow {
         void solve();
         void iterate(vector<vector<double> >&, vector<vector<double> >&, double&, double&, double&, double&, int&);
         vector<double> case_1_riemann(double, double, double, double, double, double, double, double);
+        vector<double> case_2_riemann(double, double, double, double, double, double, double, double);
         double calculate_pstar(double, double, double, double, double, double, double);
         double f_pstar(double, double, double);
         double f_prime_pstar(double, double, double);
